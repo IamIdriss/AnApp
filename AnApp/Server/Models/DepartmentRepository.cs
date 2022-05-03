b426af7de8a6cf07fff14fa3dd8ad00eb331f19f
Driss <idrissisamm@gmail.com>
2022-05-03 12:11:04 +0100

    Add Department Repository

--- AnApp/Server/Models/DepartmentRepository.cs
+++ AnApp/Server/Models/DepartmentRepository.cs
@@ -1,6 +1,33 @@
-﻿namespace AnApp.Server.Models
+﻿using AnApp.Shared.Data;
+using AnApp.Shared.Models;
+
+namespace AnApp.Server.Models
 {
     public class DepartmentRepository : IDepartmentRepository
     {
+        public Task<Department> AddDepartment(Department department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Department?> DeleteDepartment(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Department?> GetDepartment(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PagedResult<Department> GetDepartments(string? name, int page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Department?> UpdateDepartment(Department department)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- AnApp/Server/Models/IDepartmentRepository.cs
+++ AnApp/Server/Models/IDepartmentRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace AnApp.Server.Models
+﻿using AnApp.Shared.Data;
+using AnApp.Shared.Models;
+
+namespace AnApp.Server.Models
 {
     public interface IDepartmentRepository
     {
+        PagedResult<Department> GetDepartments(string? name, int page);
+        Task<Department?> GetDepartment(int Id);
+        Task<Department> AddDepartment(Department department);
+        Task<Department?> UpdateDepartment(Department department);
+        Task<Department?> DeleteDepartment(int Id);
     }
 }
